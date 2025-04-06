658a1b756750d0744835cf75625360f2e2874c69
Abhishek <abhirockzz@gmail.com>
2025-04-06 17:28:38 +0530

    updates

--- common/client.go
+++ common/client.go
@@ -15,3 +15,50 @@ func GetClient(endpoint string) (*azkustodata.Client, error) {
 	}
 	return client, nil
 }
+
+// const clusterNameFormat = "https://%s.kusto.windows.net/"
+
+// func CreateTable(clusterName, dbName, createTableCommand string) error {
+
+// 	endpoint := fmt.Sprintf(clusterNameFormat, clusterName)
+// 	fmt.Println("Cluster URL:", endpoint)
+
+// 	// Initialize the client
+// 	client, err := GetClient(endpoint)
+// 	if err != nil {
+// 		return fmt.Errorf("error creating Kusto client: %w", err)
+// 	}
+// 	defer client.Close()
+
+// 	ctx := context.Background()
+
+// 	_, err = client.Mgmt(ctx, dbName, kql.New("").AddUnsafe(createTableCommand))
+// 	if err != nil {
+// 		return fmt.Errorf("error executing create table: %w", err)
+// 	}
+
+// 	log.Println("table created successfully with command", createTableCommand)
+// 	return nil
+// }
+
+// func DropTable(clusterName, dbName, tableName string) error {
+// 	endpoint := fmt.Sprintf(clusterNameFormat, clusterName)
+// 	fmt.Println("Cluster URL:", endpoint)
+
+// 	// Initialize the client
+// 	client, err := GetClient(endpoint)
+// 	if err != nil {
+// 		return fmt.Errorf("error creating Kusto client: %w", err)
+// 	}
+// 	defer client.Close()
+
+// 	ctx := context.Background()
+
+// 	_, err = client.Mgmt(ctx, dbName, kql.New(". drop table ").AddUnsafe(tableName))
+// 	if err != nil {
+// 		return fmt.Errorf("error executing drop table command: %w", err)
+// 	}
+
+// 	log.Println("table dropped successfully", tableName)
+// 	return nil
+// }
